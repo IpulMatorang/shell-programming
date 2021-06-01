b588361b82de0cbd5546659613913d3bb314400d
IpulMatorang <85056278+IpulMatorang@users.noreply.github.com>
2021-06-01 13:14:35 +0700

    membuat file linux-command.sh

---
+++ linux-command.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#NAMA : MOH.SYAIFUL DG.MATORANG
+#NIM  : 531419017
+#KELAS: A
+echo""
+read -p "Masukan Nama Pengguna : " NAMA
+echo "selamat Datang $NAMA , Anda Masuk Sebagai Pengguna"
+echo""
+echo -e "NAMA : MOH.SYAIFUL DG.MATORANG \nNIM : 531419017 \nKELAS : A"
+echo ""
+LOKASI=$(pwd)
+DAFTAR_FILE=$(ls)
+TOTAL_FILE=$(ls -l)
+PEMAKAIAN_SPACE=$(df)
+ISI_SPACE=$(du)
+HOST_NAME=$(hostname)
+IP=$(ifconfig)
+TANGGAL=$(date)
+echo "saat ini anda berada di bagian $LOKASI"
+echo -e "Daftar file yang anda miliki saat ini :\n$DAFTAR_FILE"
+echo "Jumlah File Yang Anda Miliki Saat Ini : $TOTAL_FILE"
+echo""
+echo -e "Laporan Penggunaan Space :\n$PEMAKAIAN_SPACE"
+echo "Banyak Space Yang Digunakan Suatu File : $ISI_SPACE"
+echo""
+echo "Nama Host Yang Anda Gunakan Saat Ini : $HOST_NAME"
+echo -e "Tampilan Konfigurasi Jaringan :\n$IP"
+echo""
+echo "Tanggal Saat Ini : $TANGGAL"
+echo""
+read -p "Masukan Nama File Untuk Dieksekusi(jika tdk ada Kosongkan/langsung tekan enter) : " $FILE
+./$FILE
