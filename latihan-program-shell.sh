a2f4a3904468ec6e1ba6ddedb77de10377e83662
IpulMatorang <85056278+IpulMatorang@users.noreply.github.com>
2021-06-01 13:36:53 +0700

    membuat file latihan-program-shell.sh

---
+++ latihan-program-shell.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "Tugas 1 Progamming Shell "
+#NAMA : MOH.SYAIFUL DG.MATORANG
+#NIM  : 531419017
+echo ""
+echo "Latihan Perintah Linux"
+echo""
+echo "-----Untuk Melihat isi direktori-----"
+ls
+echo "-----Untuk Mencari path/lokasi direktori-----"
+pwd
+echo "-----Untuk mendapatkan laporan tentag penggunaan disc space sistem-----"
+df
+echo "--Untuk mengecek berapa banyak space yang dugunakan suatu file/direktori--"
+du
+echo "-----Untukme nampilkan nama host/network-----"
+hostname
+echo "-----untuk melihat atau menampilkan ip addres-----"
+ifconfig
+echo "-----Untuk menampilkan tanggal-----"
+date
+echo""
+: '
+echo "Disini saya hanya menunjukan beberapa perintah saja"
+echo "untuk perintah yang lainnya bisa lansung di cari di google"
+echo "TERIMA KASIH"
+'
